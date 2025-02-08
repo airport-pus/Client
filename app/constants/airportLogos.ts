87525802125b8b3139b4a63a3999b259e5b5204c
huise0ng <shinhuiseong07@gmail.com>
2025-02-08 11:37:36 +0900

    DOCS :: 항공사 이름/로고 매핑

---
+++ app/constants/airportLogos.ts
@@ -0,0 +1,28 @@
+const airlineLogos: Record<string, string> = {
+    "대한항공": "/logos/korean-air.webp",
+    "제주항공": "/logos/jeju-air.png",
+    "아시아나항공": "/logos/asiana-air.png",
+    "진에어": "/logos/jin-air.png",
+    "알이탈리아항공": "/logos/alitalia.svg",
+    "델타항공": "/logos/delta-airlines.png",
+    "베트남항공": "/logos/vietnam-airlines.webp",
+    "비엣젯항공": "/logos/vietjet-air.webp",
+    "이스타항공": "/logos/eastar-jet.png",
+    "중화항공": "/logos/china-airlines.svg",
+    "중국동방항공": "/logos/china-eastern.webp",
+    "상해항공": "/logos/shanghai-airlines.webp",
+    "티웨이항공": "/logos/tway-air.png",
+    "중국국제항공": "/logos/air-china.svg",
+    "춘추항공": "/logos/spring-airlines.webp",
+    "캐세이퍼시픽항공": "/logos/cathay-pacific.png",
+    "홍콩익스프레스": "/logos/hk-express.png",
+    "하와이안항공": "/logos/hawaiian-airlines.png",
+    "네덜란드항공": "/logos/klm.svg",
+    "일본항공": "/logos/japan-airlines.svg",
+    "필리핀항공": "/logos/philippine-airlines.svg",
+    "타이거에어 타이완": "/logos/tigerair-taiwan.webp",
+    "싱가포르항공": "/logos/singapore-airlines.svg"
+  };
+  
+  export default airlineLogos;
+  
