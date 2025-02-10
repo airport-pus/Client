f713e85f4d5f72fa895b5571f5ebd1d5372736c0
huise0ng <shinhuiseong07@gmail.com>
2025-02-10 21:37:33 +0900

    feat :: 헤더 컴포넌트로 뺌

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 "use client"
 
 import { useState } from "react"
-import Image from "next/image"
-import Link from "next/link"
 import Footer from "./footer/page"
+import Header from "./header/page"
 import ParkingCongestion from "./components/ParkingCongestion"
 import FareCheck from "./components/FareCheck"
 
@@ -18,21 +17,7 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow p-8 bg-[#F3F4F6] font-pretendard">
         <div className="max-w-[1280px] mx-auto px-5">
-          <div className="col-span-12 flex items-center gap-4 mb-6">
-            <Image src="/logo.svg" alt="PUSAN logo" width={100} height={100} />
-            <div className="flex-grow">
-              <h1 className="text-[20px] lg:text-[24px] font-bold text-[#000000]">김해국제공항의 모든 정보를 한 곳에서,</h1>
-              <p className="text-[16px] text-[#6B7280]">
-                지금까지 <span className="text-[#215DCE] font-semibold">333</span>명이 페이지에 방문했어요.
-              </p>
-            </div>
-            <div className="flex items-center space-x-4">
-              <Link href="/dataset">
-                <div className="text-[#606060] text-[14px] ml-4 cursor-pointer">데이터셋</div>
-              </Link>
-              <div className="text-[#606060] text-[14px] ml-4">문의하기</div>
-            </div>
-          </div>
+          <Header />
 
           <div className="grid xl:grid-cols-12 gap-5 mb-5">
             <ParkingCongestion />
