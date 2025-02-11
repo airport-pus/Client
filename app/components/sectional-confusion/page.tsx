2c13a41cbe98d5013cc40eecd36aa3ee2469bd11
huise0ng <shinhuiseong07@gmail.com>
2025-02-11 22:58:42 +0900

    feat :: 보통, 혼잡, 매우 혼잡일 때 아이콘 색 추가

--- app/components/sectional-confusion/page.tsx
+++ app/components/sectional-confusion/page.tsx
@@ -15,10 +15,23 @@ type StatusData = {
   };
 };
 
+type SectionStatus = {
+  section: string;
+  status: string;
+  statusColor: string;
+};
+
 const TrafficStatus = () => {
   const message = "실시간 공항 구간별 혼잡도 확인";
   const [selectedSection, setSelectedSection] = useState("1구간");
   const [selectedDate, setSelectedDate] = useState<DateType>("오늘");
+
+  const sectionStatuses: SectionStatus[] = [
+    { section: "1구간", status: "원활", statusColor: "bg-[#E2FFEB] text-[#16A34A]" },
+    { section: "2구간", status: "보통", statusColor: "bg-[#FFFBD8] text-[#CA8A04]" },
+    { section: "3구간", status: "혼잡", statusColor: "bg-[#FFECEC] text-[#FF0000]" },
+    { section: "전체 구간", status: "매우혼잡", statusColor: "bg-[#FFECEC] text-[#FF0000]" }
+  ];
 
   const statusData: StatusData = {
     어제: {
@@ -109,12 +122,14 @@ const TrafficStatus = () => {
             <div className="font-medium">혼잡도</div>
           </div>
           <div className="border border-gray-200 rounded-b-lg">
-            {["1구간", "2구간", "3구간", "전체 구간"].map((item, index) => (
-              <div key={index} onClick={() => setSelectedSection(item)}
+            {sectionStatuses.map((sectionStatus, index) => (
+              <div key={index} onClick={() => setSelectedSection(sectionStatus.section)}
                 className="grid grid-cols-2 p-4 border-b last:border-b-0 text-center hover:bg-[#F3F3F3] transition-colors duration-200 cursor-pointer">
-                <div className="font-medium text-[#000000]">{item}</div>
+                <div className="font-medium text-[#000000]">{sectionStatus.section}</div>
                 <div>
-                  <span className={`${sizeClass} bg-[#E2FFEB] text-[#16A34A]`}>원활</span>
+                  <span className={`${sizeClass} ${sectionStatus.statusColor}`}>
+                    {sectionStatus.status}
+                  </span>
                 </div>
               </div>
             ))}
@@ -125,7 +140,7 @@ const TrafficStatus = () => {
         <div className="mb-2 text-[22px] text-[#000000] font-bold ml-2 mt-[-64] mb-[32] flex justify-between items-center">
           <div>{`${selectedSection} 혼잡도 그래프`}</div>
           <div className="flex items-center">
-            <button   
+            <button 
               onClick={() => setSelectedDate("어제")}
               className={`px-4 py-2 text-[14px] font-medium border ${selectedDate === "어제" ? 'bg-[#EFF6FF] text-[#4F5561] border-[#BFDBFE]' : 'bg-[#F2F2F2] text-[#7A7A7A] border-[#D1D5DB]'} rounded-md flex items-center mr-2`}>
               어제
