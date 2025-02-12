bcae5122c96edc9a91f80b959d5508622f66de64
gyumingim <a316205045@gmail.com>
2025-02-12 17:41:54 +0900

    add::farecheck에 디자인 시스템을 적용했습니다 (딴곳은 안함)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,15 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        
+        gray500: '#4F5561',
+        gray575: '#757575',
+        gray600: '#6B7280',
+
+        blue100: '#EFF6FF',
+        blue200: '#D2E4FF',
+        blue300: '#BFDBFE',
+        blue500: '#215DCE',
       },
       fontFamily: {
         pretendard: ["Pretendard", "sans-serif"], 
