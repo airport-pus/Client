34344cc26e776867969ddf6ba52cfb3c74fe9b77
huise0ng <shinhuiseong07@gmail.com>
2025-02-17 19:37:51 +0900

    chore :: 잘못 입력된 텍스트 수정

--- app/components/finsh-imformation/page.tsx
+++ app/components/finsh-imformation/page.tsx
@@ -147,8 +147,8 @@ export default function StartInformation() {
   return (
     <>
       <div className="mt-14 p-4 border-l-4 border-blue500 bg-blue100 text-black mt-5">
-        <p className="font-bold text-[19px]">출발 주기장 이용 안내</p>
-        <p className="text-[16px] mt-2">• 이 주기장은 김해국제공항에서 출발하는 항공기의 주기장입니다.</p>
+        <p className="font-bold text-[19px]">도착 주기장 이용 안내</p>
+        <p className="text-[16px] mt-2">• 이 주기장은 김해국제공항에 도착하는 항공기의 주기장입니다.</p>
         <p className="text-[16px] mt-1">• 항공편명을 검색하여 원하는 항공편 정보를 쉽게 확인할 수 있습니다.</p>
         <p className="text-[16px] mt-1">• <strong>예정</strong>: 계획된 출발 시간</p>
         <p className="text-[16px] mt-1">• <strong>변경</strong>: 변경된 출발 시간</p>
