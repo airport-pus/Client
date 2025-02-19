0f57d7102e8721a3988625dca840d9babbd0ab5f
huise0ng <shinhuiseong07@gmail.com>
2025-02-19 14:13:56 +0900

    chore :: 주차조회와 타이틀 크기 및  앞라인 맞춤

--- app/components/ParkingCongestion/page.tsx
+++ app/components/ParkingCongestion/page.tsx
@@ -129,25 +129,29 @@ const ParkingCongestion = () => {
   }
 
   return (
-    <div className="col-span-12 xl:col-span-8 bg-white rounded-[8px] h-[300px] xl:h-[250px] w-full xl:w-[520px] p-4 xl:p-6 relative mt-1 md:mt-0">
-      <h2 className="text-xl font-bold mb-2 text-black text-[20px]">공항 주차장 혼잡도</h2>
-      <div className="text-gray400 mb-2 mt-[-8px] text-[14px] flex flex-col xl:flex-row xl:items-center space-y-2 xl:space-y-0">
-        <span>
-          <span className="text-blue500 underline">{bestParkingName}</span>을 이용하는게 좋겠어요.
-        </span>
-        <div className="flex items-center xl:ml-20">  
-          <span className="w-3 h-3 rounded-full bg-green500"></span>
-          <span className="text-xs ml-1">원활</span>
-          <span className="w-3 h-3 rounded-full bg-yellow300 ml-4"></span>
-          <span className="text-xs ml-1">보통</span>
-          <span className="w-3 h-3 rounded-full bg-red500 ml-4"></span>
-          <span className="text-xs ml-1">혼잡</span>
-        </div>
-      </div>
-      <div className="h-[180px] xl:h-[150px] w-full">
-        <Line data={chartData} options={chartOptions} />
-      </div>
+<div className="col-span-12 xl:col-span-8 bg-white rounded-[8px] h-[300px] xl:h-[250px] w-full xl:w-[520px] p-4 xl:p-6 relative mt-1 md:mt-0">
+  <h2 className="text-xl font-bold mt-[8px] mb-[8px] xl:mb-2 text-black text-[20px] ml-[10px] xl:ml-0">
+    공항 주차장 혼잡도
+  </h2>
+  <div className="text-gray400 mb-2 mt-[-8px] text-[14px] flex flex-col xl:flex-row xl:items-center space-y-2 xl:space-y-0">
+    <span className="ml-[10px] xl:ml-0">
+      <span className="text-blue500 underline">{bestParkingName}</span>을 이용하는게 좋겠어요.
+    </span>
+    <div className="flex items-center ml-[10px] xl:ml-20">
+      <span className="w-3 h-3 rounded-full bg-green500"></span>
+      <span className="text-xs ml-1">원활</span>
+      <span className="w-3 h-3 rounded-full bg-yellow300 ml-4"></span>
+      <span className="text-xs ml-1">보통</span>
+      <span className="w-3 h-3 rounded-full bg-red500 ml-4"></span>
+      <span className="text-xs ml-1">혼잡</span>
     </div>
+  </div>
+  <div className="h-[180px] xl:h-[150px] w-full">
+    <Line data={chartData} options={chartOptions} />
+  </div>
+</div>
+
+
   )
 }
 
