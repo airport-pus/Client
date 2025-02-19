c437ec92878f0b463064b89eddc135863b0b3078
huise0ng <shinhuiseong07@gmail.com>
2025-02-19 21:36:10 +0900

    chore :: 도착지 부분 바껴있는 항목 수정

--- app/components/start-information/page.tsx
+++ app/components/start-information/page.tsx
@@ -27,7 +27,7 @@ export default function StartInformation() {
       ? data.map((flight) => ({
           airline: flight.airlineKorean,
           flightNumber: flight.flightNumber,
-          destination: flight.boardingKor,
+          destination: flight.arrivedKor,
           gate: flight.gate || "-",
           status: flight.remarkKor || "-",
           scheduledTime: formatTime(flight.std),
@@ -87,7 +87,7 @@ export default function StartInformation() {
           {
             airline: flight.airlineKorean,
             flightNumber: flight.flightNumber,
-            destination: flight.boardingKor,
+            destination: flight.arrivedKor,
             gate: flight.gate || "-",
             status: flight.remarkKor || "-",
             scheduledTime: formatTime(flight.std),
@@ -124,9 +124,23 @@ export default function StartInformation() {
   // 정상 화면
   return (
     <>
-      <div className="mt-14 p-4 border-l-4 border-blue500 bg-blue100 text-black">
+      <div className="mt-14 p-4 border-l-4 border-blue500 bg-blue100 text-black mt-5">
         <p className="font-bold text-[19px]">출발 주기장 이용 안내</p>
-        <p className="text-[16px] mt-2">• 이 주기장은 김해국제공항에서 출발하는 항공기의 주기장입니다.</p>
+        <p className="text-[16px] mt-2">
+          • 이 주기장은 김해국제공항에 도착하는 항공기의 주기장입니다.
+        </p>
+        <p className="text-[16px] mt-1">
+          • 항공편명을 검색하여 원하는 항공편 정보를 쉽게 확인할 수 있습니다.
+        </p>
+        <p className="text-[16px] mt-1">
+          • <strong>예정</strong>: 계획된 출발 시간
+        </p>
+        <p className="text-[16px] mt-1">
+          • <strong>변경</strong>: 변경된 출발 시간
+        </p>
+        <p className="text-[16px] text-red500 mt-1">
+          • <strong>빨간색 표시</strong>: 항공편이 지연된 경우, 지연 시간을 함께 표시합니다.
+        </p>
         <div className="mt-4">
           <div className="relative">
             <Image
