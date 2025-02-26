7c9c2bec014f8fe0ed6b20ec879c383104c44147
huise0ng <shinhuiseong07@gmail.com>
2025-02-26 23:39:20 +0900

    chore :: api에서 주기장 정보가 없을 경우에 주기장에 대한 항공편 정보가 없습니다. 텍스트 띄움

--- app/components/finsh-information/page.tsx
+++ app/components/finsh-information/page.tsx
@@ -249,11 +249,18 @@ export default function StartInformation() {
           <div>항공편 상태</div>
           <div>시간</div>
         </div>
+
+        {displayedFlights.length === 0 && !inputValue && (
+          <div className="text-center text-gray700 mt-8 mb-4">
+            도착 주기장에 대한 항공편 정보가 없습니다.
+          </div>
+        )}
         {displayedFlights.length === 0 && inputValue && (
           <div className="text-center text-gray700 mt-8 mb-4">
             검색한 항공편에 대한 정보가 없습니다.
           </div>
         )}
+
         <StartData
           displayedFlights={displayedFlights}
           lastFlightElementRef={lastFlightElementRef}
@@ -261,11 +268,6 @@ export default function StartInformation() {
       </div>
 
       <div className="md:hidden divide-y divide-gray-300">
-        {displayedFlights.length === 0 && inputValue && (
-          <div className="text-center text-gray700 mt-8 mb-4">
-            검색한 항공편에 대한 정보가 없습니다.
-          </div>
-        )}
         {displayedFlights.map((flight, index) => (
           <div key={`${flight.flightNumber}-${index}`} className="bg-white p-4">
             <div className="flex justify-between items-center mb-2">
