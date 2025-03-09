a71e415fc1113492bfe1de065c3e3afb00055177
siniseong <shinhuiseong07@gmail.com>
2025-03-10 08:51:03 +0900

    feat :: google analytics 설정 추가

---
+++ app/components/GoogleAnalytics.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { usePathname, useSearchParams } from 'next/navigation';
+import Script from 'next/script';
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
+  }
+}
+
+export default function GoogleAnalytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (pathname) {
+      window.gtag('event', 'page_view', {
+        page_path: pathname + searchParams.toString(),
+        page_title: document.title,
+        page_location: window.location.href,
+      });
+    }
+  }, [pathname, searchParams]);
+
+  useEffect(() => {
+    const visitStartTime = new Date();
+
+    const recordVisitDuration = () => {
+      const visitEndTime = new Date();
+      const visitDuration = visitEndTime.getTime() - visitStartTime.getTime();
+      
+      window.gtag('event', 'visit_duration', {
+        value: Math.round(visitDuration / 1000),
+        page_path: pathname + searchParams.toString(),
+      });
+    };
+
+    window.addEventListener('beforeunload', recordVisitDuration);
+    return () => {
+      window.removeEventListener('beforeunload', recordVisitDuration);
+    };
+  }, [pathname, searchParams]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-Z17J22R36S`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Z17J22R36S', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+}
