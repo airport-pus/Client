977b8dbc21c152ae4febce02ad7acb50bad69e78
siniseong <shinhuiseong07@gmail.com>
2025-03-10 09:07:18 +0900

    fix :: 환경변수로 뺌

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -58,11 +58,17 @@ function AnalyticsContent() {
 }
 
 export default function GoogleAnalytics() {
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+  if (!GA_ID) {
+    return null;
+  }
+
   return (
     <>
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-Z17J22R36S`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
       />
       <Script
         id="google-analytics"
@@ -72,7 +78,7 @@ export default function GoogleAnalytics() {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-Z17J22R36S', {
+            gtag('config', '${GA_ID}', {
               page_path: window.location.pathname,
             });
           `,
